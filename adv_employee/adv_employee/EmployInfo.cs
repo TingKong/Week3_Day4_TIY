ef4be253e4a1dba3fa8412230127d43342e640ef
Ting <Ting@TINGKONG7B77>
2016-10-13 13:58:20 -0400

    Create new xml files

---
+++ adv_employee/adv_employee/EmployInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adv_employee
+{
+   public class EmployInfo
+    {
+        public EmployInfo()
+        {
+        }
+
+        public EmployInfo(string id, string name, double payrate)
+        {
+          
+        }
+
+        public string id { get;  }
+        public string name { get;  }
+        public double payrate { get; }
+    }
+}
---
+++ adv_employee/adv_employee/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+
+namespace adv_employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<EmployInfo> employlist = new List<EmployInfo>();
+
+            string fileName = "C:\\Users\\Ting\\Source\\Repos\\Week3_Day4_TIY\\employees.xml";
+
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine("Found it");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+                Console.WriteLine("Let me create the employee file for you");
+                using (XmlWriter writer = XmlWriter.Create(fileName))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Profiles");
+
+                    foreach (EmployInfo b in employlist)
+                    {
+                        writer.WriteStartElement("Employee");
+
+                        writer.WriteElementString("Id", b.id);
+                        writer.WriteElementString("Name", b.name);
+                        writer.WriteElementString("Payrate", Convert.ToString(b.payrate));
+
+
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+
+                }
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
